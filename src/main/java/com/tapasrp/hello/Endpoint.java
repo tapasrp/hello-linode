d7d4604bf83ecb9381c51868a6ea9d29423e79d9
Tapas Pati <45442732+tapasrp@users.noreply.github.com>
2023-03-23 23:58:41 -0400

    first commit

---
+++ src/main/java/com/tapasrp/hello/Endpoint.java
@@ -0,0 +1,13 @@
+package com.tapasrp.hello;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Endpoint {
+
+    private String name;
+    private String value;
+
+}
